6a6d9e5d6be2b9639441d1b04cd8d2bbaf66883e
James Harris <jimbo.harris@gmail.com>
2025-04-28 18:12:25 +0100

    fixed clap specification

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -18,18 +18,34 @@ fn run_cmd(cmd: &[&str]) -> Result<()> {
     }
 }
 
-
+fn dump_cmd(indent: usize, cmd: &clap::Command) {
+    println!("{}app: {}, {} subcommands, {} args", " ".repeat(indent*2), cmd.get_name(), cmd.get_subcommands().count(), cmd.get_arguments().count());
+    for a in cmd.get_arguments() {
+        println!("{}{} {} {} {} {   }", 
+            " ".repeat(indent*2+4),
+            a.get_id(),
+            match a.get_short() {Some(s) => format!("{}",s), None => String::from("-")},
+            match a.get_long() {Some(s) => s.into(), None => "-"},
+            match a.get_num_args() {Some(s) => format!("{}",s), None => String::from("-")},
+            match a.get_help() {Some(s) => format!("{}",s), None => String::from("-")},
+        );
+    }
+    
+    for c in cmd.get_subcommands() {
+        dump_cmd(indent+1, c);
+    }
+        
+}
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     
     
     let app = make_app();
+    dump_cmd(0, &app);
     
-    for c in &app.subcommands {
-        println!("{:?}", c);
-    }
     
-    run_cmd(&["osmquadtree-utils", "count", "/home/james/data/planet-feb2025/"])?;
+    
+    run_cmd(&["osmquadtree-utils", "count", "/home/james/data/planet-feb2025/", "--filter", "-1.5,49.5,3.0,52.5"])?;
     
     Ok(())
 }
